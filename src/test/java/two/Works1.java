9bf2c64018d6b623a67d1191a79c1c286d59cb22
karthick77 <mail2bkarthick@gmail.com>
2024-06-26 16:20:12 +0530

    city

--- src/test/java/two/Works1.java
+++ src/test/java/two/Works1.java
@@ -6,7 +6,7 @@ public class Works1 {
 	private double emp_id;
 	private int age;
 	private double salary;
-	private String city;
+	private String city;//city
  
 	public static void main(String[] args) {
 		
