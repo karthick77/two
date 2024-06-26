f7c6f2cea21014967fc54b87f29a2091dfb7185e
karthick77 <mail2bkarthick@gmail.com>
2024-06-26 15:26:28 +0530

    Works1 masters 1234

--- src/test/java/two/Works1.java
+++ src/test/java/two/Works1.java
@@ -6,7 +6,7 @@ public class Works1 {
 		
 		System.out.println("Works1 from branchX");
 		System.out.println("Works2 from branchX");
-		System.out.println("Dream1 from master");
+		System.out.println("Dream1 from masters 1234");
 
 
 	}
