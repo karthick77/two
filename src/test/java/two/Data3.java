287346f87834662cd461dac47ffe82867893347f
karthick77 <mail2bkarthick@gmail.com>
2024-06-26 13:04:40 +0530

    Second branchX commit

---
+++ src/test/java/two/Data3.java
@@ -0,0 +1,16 @@
+package two;
+
+public class Data3 {
+
+	public static void main(String[] args) {
+		
+		String hungry = "Yes";
+		
+		if(hungry == "Yes"||hungry == "Y"||hungry == "y") {
+			System.out.println("Start eating....");
+		}else {
+			System.out.println("Finish eating...");
+		}
+
+	}
+}
